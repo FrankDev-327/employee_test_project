460554d7647fad4e83d818b6c395ad71711d5a87
Francisco Gonzalez <salutacinasfer@hotmail.com>
2023-07-12 23:03:47 +0200

    moving file into folders

--- api/read.php
+++ api/read.php
@@ -2,8 +2,8 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-include_once './db_config/database.php';
-include_once './classes/employees.php';
+include_once '../db_config/database.php';
+include_once '../classes/employees.php';
 
 $database = new Database();
 $db = $database->getConnectionDB();
--- classes/employees.php
+++ classes/employees.php
@@ -1,4 +1,5 @@
 <?php
+include_once '../interfaces/employee-interfacce.php';
 class Employee implements EmployeeInterface {
     // Connection
     private $conn;
